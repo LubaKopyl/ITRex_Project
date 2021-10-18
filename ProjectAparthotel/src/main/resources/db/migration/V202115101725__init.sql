27d24f0dcfffef2264134b088ba43dc64ff94950
Luba Kopyl <luba.kopylll@gmail.com>
2021-10-18 22:38:37 +0300

    changed table names

--- ProjectAparthotel/src/main/resources/db/migration/V202115101725__init.sql
+++ ProjectAparthotel/src/main/resources/db/migration/V202115101725__init.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS aparthotel.users (
+CREATE TABLE IF NOT EXISTS users (
 user_id INT NOT NULL,
 first_name VARCHAR(50) NOT NULL,
 last_name VARCHAR(50) NOT NULL,
@@ -8,25 +8,25 @@ PRIMARY KEY (user_id),
 UNIQUE (email)
 );
 
-CREATE TABLE IF NOT EXISTS aparthotel.roles (
+CREATE TABLE IF NOT EXISTS roles (
 role_id INT NOT NULL,
 guest BOOLEAN NOT NULL,
 admin BOOLEAN NOT NULL,
 PRIMARY KEY (role_id)
 );
 
-CREATE TABLE IF NOT EXISTS aparthotel.users_roles (
+CREATE TABLE IF NOT EXISTS users_roles (
 user_id INT NOT NULL,
 role_id INT NOT NULL,
-FOREIGN KEY (user_id) REFERENCES aparthotel.users (user_id)
+FOREIGN KEY (user_id) REFERENCES users (user_id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION,
-FOREIGN KEY (role_id) REFERENCES aparthotel.roles (role_id)
+FOREIGN KEY (role_id) REFERENCES roles (role_id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 );
 
-CREATE TABLE IF NOT EXISTS aparthotel.rooms (
+CREATE TABLE IF NOT EXISTS rooms (
 room_id INT NOT NULL,
 number_of_guests INT NOT NULL,
 number_of_beds INT NOT NULL,
@@ -38,7 +38,7 @@ air_conditioning BOOLEAN,
 PRIMARY KEY (room_id)
 );
 
-CREATE TABLE IF NOT EXISTS aparthotel.bookings (
+CREATE TABLE IF NOT EXISTS bookings (
 booking_id INT NOT NULL,
 room_id INT NOT NULL,
 user_id INT NOT NULL,
@@ -46,22 +46,22 @@ arrival_date TIMESTAMP NOT NULL,
 departure_date TIMESTAMP NOT NULL,
 total_price DECIMAL NOT NULL,
 PRIMARY KEY (booking_id),
-FOREIGN KEY (room_id) REFERENCES aparthotel.rooms (room_id)
+FOREIGN KEY (room_id) REFERENCES rooms (room_id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION,
-FOREIGN KEY (user_id) REFERENCES aparthotel.users (user_id)
+FOREIGN KEY (user_id) REFERENCES users (user_id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 );
 
-CREATE TABLE IF NOT EXISTS aparthotel.prices (
+CREATE TABLE IF NOT EXISTS prices (
 price_id INT NOT NULL,
 room_id INT NOT NULL,
 period_start TIMESTAMP NOT NULL,
 period_end TIMESTAMP NOT NULL,
 price DECIMAL NOT NULL,
 PRIMARY KEY (price_id),
-FOREIGN KEY (room_id) REFERENCES aparthotel.rooms (room_id)
+FOREIGN KEY (room_id) REFERENCES rooms (room_id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 );
